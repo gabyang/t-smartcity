e6490dd996fcfba1014461230ded656383835354
Bryvo <bryancastorius@gmail.com>
2025-01-13 00:43:45 +0800

    oi

--- agents/synthetic_data_agent.py
+++ agents/synthetic_data_agent.py
@@ -1,8 +1,12 @@
 from openai import OpenAI
-
-client = OpenAI()
 import re
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def parse_query(user_query: str):
     """
